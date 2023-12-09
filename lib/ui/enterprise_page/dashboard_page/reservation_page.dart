3daf358b72f6ec0e64c4caf0b8ae4a67fbeb0129
smthswt <hin19823@yonsei.ac.kr>
2023-12-10 00:29:18 +0900

    update

--- lib/ui/enterprise_page/dashboard_page/reservation_page.dart
+++ lib/ui/enterprise_page/dashboard_page/reservation_page.dart
@@ -66,7 +66,7 @@ class GetReservations extends StatelessWidget {
                             size: 50,
                           ),
                           Container(
-                            height: 80,
+                            height: 100,
                             width: 220,
                             color: Colors.blue,
                             padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
@@ -104,8 +104,9 @@ class GetReservations extends StatelessWidget {
                                 Container(
                                   height: 4,
                                 ),
-                                Text("인원 : ${data['confNum']} 명"),
-                                Text("예약명 : ${data['confName']}"),
+                                Text("인원 : ${data['person']} 명"),
+                                // Text("${data['person'] ?? 'loading...'}명"),
+                                Text("예약명 : ${data['nickname']}"),
                               ],
                             ),
                           ),
@@ -188,137 +189,3 @@ class GetReservations extends StatelessWidget {
 
 
 
-
-
-
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'dart:async';
-//
-// class GetReservations extends StatelessWidget {
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//   late String businessCode;
-//   late String confAddress;
-//   late String confName;
-//   late String confNum;
-//   final user = FirebaseAuth.instance.currentUser!;
-//
-//   final String documentID;
-//   final double bRad;
-//   final int bColor;
-//   final Function(bool, dynamic) callback;
-//
-//
-//   GetReservations({
-//     required this.documentID,
-//     required this.bRad,
-//     required this.bColor,
-//     required this.callback,
-//     required this.confAddress,
-//     required this.confName,
-//     required this.confNum,
-//
-//   });
-//
-//   // Future<void> initNotifications() async {
-//   //   // request permission from user
-//   //   await _FirebaseMessaging.requestPermission();
-//   //   // fetch FCM Token
-//   //   final fCMToken = await _FirebaseMessaging.getToken();
-//   //   // Print Token
-//   //   print('Token:  + $fCMToken');
-//   // }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     CollectionReference reservations =
-//     _firestore.collection('reservation_waiting');
-//
-//     return FutureBuilder<DocumentSnapshot>(
-//       future: reservations.doc(documentID).get(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           if (snapshot.hasData) {
-//             ////    PRINT SNAPSHOT    /////
-//             // print(snapshot);
-//
-//             if (snapshot.data == null || !snapshot.data!.exists) {
-//               // Document doesn't exist
-//               return SizedBox.shrink();
-//             }
-//             Map<String, dynamic> data =
-//             snapshot.data!.data() as Map<String, dynamic>;
-//
-//             return Container(
-//               margin: const EdgeInsets.all(2.0),
-//               decoration: BoxDecoration(
-//                 color: Color(bColor),
-//                 borderRadius: BorderRadius.circular(bRad),
-//               ),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   Icon(Icons.calendar_month, size: 50,),
-//                   Column(
-//                     mainAxisAlignment: MainAxisAlignment.start,
-//                     children: [
-//                       Text("${data['time'] ?? 'loading...'} 시"),
-//                       Text("${data['person'] ?? 'loading...'}명"),
-//                       Text('${data['user'] ?? 'loading...'}'),
-//                       Text("노쇼 : ${data['noShow'] ?? 'loading...'} 회"),
-//                     ],
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: () async {
-//                       String reservation_Id = documentID;
-//
-//                       await _firestore.collection('reservation_confirm').doc(reservation_Id).set({
-//                         'person': data['person'],
-//                         'user': data['user'],
-//                         'noShow': data['noShow'],
-//                         'nickname': data['nickname'],
-//                         'prefer': data['prefer'],
-//                         'time': data['time'],
-//                         'location': data['location'],
-//                         "pending_status": true,
-//                         "confirm_status": true,
-//                         "button_status": true,
-//                         "confirmedStoreAddress": confAddress,
-//                         "confirmedStoreName": confName,
-//                         "confirmedStoreNum": confNum,
-//                         "Enter_uid" : user.uid,
-//                         "customer_uid" : data['customer_uid'],
-//                         "reservation_Id" : reservation_Id,
-//                       });
-//
-//                       await _firestore.collection('reservation_waiting')
-//                           .doc(documentID)
-//                           .delete();
-//                       callback(true, documentID);
-//                     },
-//                     child: Text("수락"),
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       callback(true, documentID);
-//                     },
-//                     child: Text("제거"),
-//                   ),
-//                 ],
-//               ),
-//             );
-//           } else {
-//             // If snapshot has no data, return an empty Container
-//             return Container();
-//           }
-//         }
-//         return Text('');
-//       },
-//     );
-//   }
-// }
